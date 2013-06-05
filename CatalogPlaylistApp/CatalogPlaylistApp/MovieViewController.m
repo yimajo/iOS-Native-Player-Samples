17005434c67546784f22f6e4dc7c2c0eddb4631d
imajo <imajoyoshinori@imajo-osx.local>
2013-06-05 15:15:12 +0900

    検証コードをコミット。 allowsExternalPlaybackはデフォルトYES。さらにKVOで監視してもallowsExternalPlaybackがAirPlay検知時に変化しているわけではない

--- CatalogPlaylistApp/CatalogPlaylistApp/MovieViewController.m
+++ CatalogPlaylistApp/CatalogPlaylistApp/MovieViewController.m
@@ -11,9 +11,21 @@
 #import "BCVideo.h"
 #import "BCQueuePlayer.h"
 #import "BCEvent.h"
+#import "BCQueueDisplay.h"
+#import <AVFoundation/AVPlayer.h>
+
+@interface MovieViewController ()
+
+@property (nonatomic) BOOL flag;
+
+@end
 
 @implementation MovieViewController
 
+- (void)dealloc
+{
+    [self.player.displayComponent.activePlayer removeObserver:self forKeyPath:@"allowsExternalPlayback"];
+}
 
 - (void)viewDidLoad
 {
@@ -33,23 +45,34 @@
     //Play on player
     [self.player play];
     
-    ////////////////////////////////////////////////////
-    //AirPlayが有効かどうかをairPlayVideoActiveメソッドを実行して判断
-    ////////////////////////////////////////////////////
-    //
-    //[self.player airPlayVideoActive];
-    //
-    //airPlayVideoActiveメソッドは例外が発生し正常に実行できない。例外は下記
-    //2013-05-23 11:26:41.762 CatalogPlaylistApp[229:907] *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '-[BCQueueDisplay airPlayVideoActive]: unrecognized selector sent to instance 0x1d574b40'
-    ////////////////////////////////////////////////////
-    
-    ////////////////////////////////////////////////////
-    //AirPlayが有効かどうかをBCEventEnableAirPlayイベントを取得し判断
-    ////////////////////////////////////////////////////
-    [self.player.playbackEmitter on:BCEventEnableAirPlay callBlock:^(BCEvent *event) {
-        NSLog(@"BCEventEnableAirPlayは実行される?");
-    }];
-    ////////////////////////////////////////////////////
+    //allowsExternalPlaybackの変化をKVOで監視
+    [self.player.displayComponent.activePlayer addObserver:self forKeyPath:@"allowsExternalPlayback" options:0 context:nil];
+
+    if (self.player.displayComponent.activePlayer.allowsExternalPlayback == true) {
+        //デフォルトYESなのでこちらを通る
+        //http://developer.apple.com/library/ios/#documentation/AVFoundation/Reference/AVPlayer_Class/Reference/Reference.html
+        UIAlertView *alert =
+        [[UIAlertView alloc] initWithTitle:@"Test"
+                                   message:@"YES"
+                                  delegate:self
+                         cancelButtonTitle:@"Check"
+                         otherButtonTitles:nil];
+        [alert show];
+    } else {
+        UIAlertView *alert =
+        [[UIAlertView alloc] initWithTitle:@"Test"
+                                   message:@"NO"
+                                  delegate:self
+                         cancelButtonTitle:@"Check"
+                         otherButtonTitles:nil];
+        [alert show];
+        
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    NSLog(@"func %s: object %@, changed keypath %@, change %@", __func__, object, keyPath, change);
 }
 
 @end
